353c5ffc1c8552ea4b2d3eb734b764110e984437
Sandeep <sandeepeos04@gmail.com>
2024-09-23 14:36:03 +0530

    Finished basic backend

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://sandy:greatnessiscoming9604@cluster0.nipxquz.mongodb.net/"
+  "mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos"
 );
 const todoSchema = mongoose.Schema({
   title: String,
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { createToDo, editToDo } = require("./types");
+const { todo } = require("./db");
+
 const app = express();
 app.use(express.json());
 
@@ -7,22 +9,36 @@ app.get("/", (req, res) => {
   res.send("<h1>Todo App</h1>");
 });
 
-app.post("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
   const createPayLoad = req.body;
   const parsedPayLoad = createToDo.safeParse(createPayLoad);
   if (!parsedPayLoad.success) {
     res.status(411).json({ msg: "Wrong Input Entered" });
     return;
   }
-  res.send("<h1>Todo App</h1>");
+  await todo.create({
+    title: createPayLoad.title,
+    description: createPayLoad.description,
+    completed: false,
+  });
+  res.json({ msg: "Todo was created" });
 });
 
-app.get("/todos", (req, res) => {
-  res.send("<h1>Todo App</h1>");
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find({});
+  res.json({ todos: todos });
 });
 
-app.put("/completed", (req, res) => {
-  res.send("<h1>Todo App</h1>");
+app.put("/completed", async (req, res) => {
+  const createPayLoad = req.body;
+  const parsedPayLoad = createToDo.safeParse(createPayLoad);
+  if (!parsedPayLoad.success) {
+    res.status(411).json({ msg: "Wrong Input Entered" });
+    return;
+  }
+  await todo.update({ _id: req.body.id }, { completed: true });
+
+  res.json({ msg: "Todo was updated" });
 });
 
 app.listen(3000, () => {
